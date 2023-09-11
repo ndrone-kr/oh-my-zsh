82b9e6ae4814156ad834a61e556d7e742f0eccbb
Nicholas Drone <ndrone@gmail.com>
2023-09-11 17:44:07 -0400

    Updates zsh-syntax-highlighting install path

--- custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+++ custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
@@ -1,1 +1,2 @@
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# $HOMEBREW_PREFIX=/opt/homebrew
+source $HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
